df069ed566befc4887901dbb38729d36752226c8
huangyijian <1172256619@qq.com>
2019-10-17 18:44:38 +0800

    初始项目

---
+++ prompts.js
@@ -0,0 +1,15 @@
+module.exports = [{
+    type: 'list',
+    name: 'template',
+    message: '请选择 uni-app 模板',
+    choices: [{
+        name: '默认模板',
+        value: 'default'
+    }, {
+        name: '这是啥模板',
+        value: 'default'
+    }
+    ],
+    default: 'None'
+}
+]
